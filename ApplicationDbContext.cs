0cdbd6284295e64b5cf6efedac2ba1f9592041ff
邓泽宇 <393911462@qq.com>
2018-05-10 10:55:32 +0800

    init

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnet_mysql
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
