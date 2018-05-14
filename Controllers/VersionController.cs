63134712a0278dbe16ff9d51265182af5c8f7c49
邓泽宇 <399311462@qq.com>
2018-05-14 10:21:59 +0800

    修改了代码逻辑 能够很好的使用 EF 框架

--- Controllers/VersionController.cs
+++ Controllers/VersionController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using aspnet_mysql.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,14 +12,24 @@ namespace aspnetmysql.Controllers
 {
     public class VersionController : Controller
     {
+    //      static string connection = @"Server=cd-cdb-2b19h0iv.sql.tencentcdb.com;Port=63990;database=ef;uid=root;pwd=dqx930215115;";
+    //     static DbContextOptions<MyContext> dbContextOption = new DbContextOptions<MyContext>();
+    //     static DbContextOptionsBuilder<MyContext> dbContextOptionBuilder = new DbContextOptionsBuilder<MyContext>(dbContextOption);
+       MyContext _Context;
+	    // = new MyContext(dbContextOptionBuilder.UseSqlServer(connection).Options);
+		public VersionController(MyContext context)
+		{
+			_Context = context;
+		}
         public string Index()
 		{
-			return "0.0.5";
+			return "0.0.6 local";
 		}
         public string getUserName()
 		{
-			return "AA";
+			return _Context.Users.Find(1).Name;
+			
 		}
-
-    }
+	}
+ 
 }
